9b93fdd3fb644327e904dfa3906d461cbbb5ea9d
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-04-07 02:47:20 +0300

    feat: move from gitlab

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# gitlab-errand-boy
+# gitlab_errand_boy
+
+### Usage
+
+```python
+import gitlab_errand_boy
+
+
+compounder = gitlab_errand_boy.Compounder(
+    project_id="23609881",
+    api_token="3Tsfsbuk9464TrdtrNNd"
+)
+
+# Create compound MR from open MRs.
+compounder.compound()
+```
+
+### Requirements
+
+Requires Python >= 3.9.
---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Set an alias run='./run.sh'
+
+set -eo pipefail
+
+DC="${DC:-exec}"
+
+# If we're running in CI we need to disable TTY allocation for docker-compose
+# commands that enable it by default, such as exec and run.
+TTY=""
+if [[ ! -t 1 ]]; then
+  TTY="-T"
+fi
+
+# -----------------------------------------------------------------------------
+# Helper functions start with _ and aren't listed in this script's help menu.
+# -----------------------------------------------------------------------------
+
+
+
+# -----------------------------------------------------------------------------
+
+function publish {
+  poetry build && poetry publish
+}
+
+function help {
+  printf "%s <task> [args]\n\nTasks:\n" "${0}"
+
+  compgen -A function | grep -v "^_" | cat -n
+
+  printf "\nExtended help:\n  Each task has comments for general usage\n"
+}
+
+TIMEFORMAT=$'\nTask completed in %3lR'
+time "${@:-help}"
